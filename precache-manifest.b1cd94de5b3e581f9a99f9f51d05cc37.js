f451e567c64134745fbee11e7d5880e8d01c6736
abramlek22 <64551269+abramlek22@users.noreply.github.com>
2020-09-28 14:11:25 +0300

    Updates

---
+++ precache-manifest.b1cd94de5b3e581f9a99f9f51d05cc37.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082b27d3719450eb746b4965199ccc5c",
+    "url": "/Lekhman-vold/demo-social-app/index.html"
+  },
+  {
+    "revision": "9743a9bbe6736f712509",
+    "url": "/Lekhman-vold/demo-social-app/static/css/3.8bf98a2b.chunk.css"
+  },
+  {
+    "revision": "e6a9bdd211eb3ea3f1d6",
+    "url": "/Lekhman-vold/demo-social-app/static/css/4.9463e488.chunk.css"
+  },
+  {
+    "revision": "8cd064e935a3fadeca5b",
+    "url": "/Lekhman-vold/demo-social-app/static/css/main.6623ba35.chunk.css"
+  },
+  {
+    "revision": "8492731e65f2810da654",
+    "url": "/Lekhman-vold/demo-social-app/static/js/2.66209f6d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Lekhman-vold/demo-social-app/static/js/2.66209f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9743a9bbe6736f712509",
+    "url": "/Lekhman-vold/demo-social-app/static/js/3.2817c612.chunk.js"
+  },
+  {
+    "revision": "e6a9bdd211eb3ea3f1d6",
+    "url": "/Lekhman-vold/demo-social-app/static/js/4.91148220.chunk.js"
+  },
+  {
+    "revision": "8cd064e935a3fadeca5b",
+    "url": "/Lekhman-vold/demo-social-app/static/js/main.1c9a7ee7.chunk.js"
+  },
+  {
+    "revision": "ec4a11a61dd71e3abb3d",
+    "url": "/Lekhman-vold/demo-social-app/static/js/runtime-main.dad64312.js"
+  },
+  {
+    "revision": "7d43e5f117fb0802c03e3e8c2cbf9c71",
+    "url": "/Lekhman-vold/demo-social-app/static/media/Us.7d43e5f1.jpg"
+  },
+  {
+    "revision": "987d4a2e03f3704b712f0d3c10cf67fb",
+    "url": "/Lekhman-vold/demo-social-app/static/media/User.987d4a2e.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Lekhman-vold/demo-social-app/precache-manifest.b1cd94de5b3e581f9a99f9f51d05cc37.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Lekhman-vold/demo-social-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
